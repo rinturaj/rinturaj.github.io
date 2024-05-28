cb9476e264b088112ab70caae92ce7c11d66a6b3
Rintu Raj.c <rinturajc180000@gmail.com>
2024-05-28 13:21:04 +0530

    terminal view

--- main.js
+++ main.js
@@ -1,58 +1,52 @@
-import "./style.css";
-import * as THREE from "three";
+document.addEventListener("DOMContentLoaded", () => {
+  const terminalInput = document.getElementById("terminal-input");
+  const terminal = document.getElementById("terminal");
+  const terminalContent = document.getElementById("terminal-content");
+  const currentInput = document.getElementById("current-input");
+  console.log(terminalContent);
+  terminal.addEventListener("click", () => {
+    terminalInput.focus();
+    console.log("terminal input focus");
+  });
+  terminalInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      const inputText = terminalInput.value;
+      terminalInput.value = "";
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+      if (inputText.trim() !== "") {
+        const userInputLine = document.createElement("p");
+        userInputLine.classList.add("line");
+        userInputLine.textContent = `user@computer:~$ ${inputText}`;
+        terminalContent.insertBefore(
+          userInputLine,
+          terminalContent.lastElementChild
+        );
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("#bg"),
+        const outputLine = document.createElement("p");
+        outputLine.classList.add("line");
+        outputLine.textContent = executeCommand(inputText);
+        terminalContent.insertBefore(
+          outputLine,
+          terminalContent.lastElementChild
+        );
+      }
+
+      currentInput.textContent = "";
+    } else if (event.key === "Backspace") {
+      currentInput.textContent = currentInput.textContent.slice(0, -1);
+    } else if (event.key.length === 1) {
+      currentInput.textContent += event.key;
+    }
+  });
 });
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-camera.position.setX(-3);
-
-const pmremGenerator = new THREE.PMREMGenerator(renderer);
-
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xff5d0f);
-// scene.environment = pmremGenerator.fromScene(
-//   new RoomEnvironment(),
-//   0.04
-// ).texture;
-renderer.render(scene, camera);
-
-const geometry = new THREE.PlaneGeometry( 1, 1 );
-const textureColor = new THREE.TextureLoader().load( 'assets/texture/Leather_007_ROUGH.jpg' );
-const textureNorm = new THREE.TextureLoader().load( 'assets/texture/Paper_Recycled_001_NORM.jpg' );
-const material = new THREE.MeshBasicMaterial( { map:textureColor,color:0xa5500f } );
-
-const plane = new THREE.Mesh( geometry, material );
-scene.add( plane );
-plane.position.z = -1;
-const plane1 = new THREE.Mesh( geometry, material );
-scene.add( plane1 );
-plane1.position.z = +1;
-
-function moveCamera() {
-  const t = document.body.getBoundingClientRect().top;
-  camera.position.z = t * -0.01;
-  camera.position.x = t * -0.0002;
-  camera.rotation.y = t * -0.0002;
+function executeCommand(command) {
+  switch (command) {
+    case "echo 'Hello, world!'":
+      return "Hello, world!";
+    case "date":
+      return new Date().toString();
+    default:
+      return `command not found: ${command}`;
+  }
 }
-document.body.onscroll = moveCamera;
-moveCamera();
-
-// Animation Loop
-
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-}
-
-animate();
